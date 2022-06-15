8bd3e40bca2f9cf7dc5473bcaa9f9145c7fecf82
Ian <yeon.kim.0317@gmail.com>
2022-06-15 16:49:34 +0900

    inp test

--- js/hmui/ui_common.js
+++ js/hmui/ui_common.js
@@ -278,16 +278,6 @@ var _inpControl = function() {
           });
           _resetFixedBtnPos();
       }
-
-      // if ( checkParent && $(e.target).parents('.wrap_chk').length ) {
-      //   setTimeout(function(){
-      //     inp.each(function(){
-      //       $(this).blur();
-      //       $(this).closest('.box_inp').removeClass('show_btn');
-      //     });
-      //     _resetFixedBtnPos();
-      //   },100);
-      // }
     });
   }
 
